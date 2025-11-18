d2216f02fe9ff4ed2373ca86d48ed7f16ae7dbb2
NikushM <nmedentsiy@gmail.com>
2025-11-18 19:20:32 +0100

    feat: add WeatherTestView for debugging

---
+++ WeatherNotes/App/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Nikush on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WeatherView()
+        }
+    }
+}
